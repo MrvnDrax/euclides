aebaa434378f24b02eeafd3f299cb83f6178aac4
MrvnDrax <emiliano.meza.ochoa@icloud.com>
2025-07-09 13:22:20 -0600

    update euclides

---
+++ main.py
@@ -0,0 +1,21 @@
+
+
+
+# Una forma visual del funcionamiento del algoritmo de euclides #
+
+a = int(input("Ingresa tu primer numero:"))
+b = int(input("Ingresa tu segundo numero:"))
+
+
+def mcd(m, d):
+    print("Listo para ver cómo funciona el algoritmo?, espero que sí")
+
+    while d != 0:
+        q = m // d
+        r = m % d
+        print(f"MCD({m}, {d}): {m} = {q} × {d} + {r}")
+        m, d = d, r
+    return m
+
+resultado = mcd(a, b)
+print(f"El MCD({a}, {b}) = {resultado}")
